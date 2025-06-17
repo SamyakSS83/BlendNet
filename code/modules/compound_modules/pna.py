d1a66457ec3a11436e30ff5f70b2079f14a1ec9f
Samyak Sanghvi <161827338+SamyakSS83@users.noreply.github.com>
2025-06-18 00:50:26 +0530

    torch-np errors

--- code/modules/compound_modules/pna.py
+++ code/modules/compound_modules/pna.py
@@ -56,12 +56,12 @@ def scale_identity(h, D=None, avg_d=None):
 
 def scale_amplification(h, D, avg_d):
     # log(D + 1) / d * h     where d is the average of the ``log(D + 1)`` in the training set
-    return h * (np.log(D + 1) / avg_d["log"])
+    return h * (torch.log(D + 1) / avg_d["log"])
 
 
 def scale_attenuation(h, D, avg_d):
     # (log(D + 1))^-1 / d * X     where d is the average of the ``log(D + 1))^-1`` in the training set
-    return h * (avg_d["log"] / np.log(D + 1))
+    return h * (avg_d["log"] / torch.log(D + 1))
 
 
 PNA_AGGREGATORS = {
